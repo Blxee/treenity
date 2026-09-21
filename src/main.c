54ddb0c2866b5c7206c675a1319e1f66886e3493
Blxee <aminedev57@gmail.com>
2026-09-21 09:49:47 +0000

    Added: some basic hello world

--- src/main.c
+++ src/main.c
@@ -1,8 +1,26 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 #include <stdio.h>
 #include "treenity.h"
 
+void handler(int sig)
+{
+  (void)sig;
+  unlink("treenity");
+  printf("exotting..\n");
+  exit(0);
+}
+
 int main(void)
 {
-  printf("Hello, World!");
+  struct sigaction sa = {0};
+  sa.sa_handler = &handler;
+  sigaction(SIGINT, &sa, NULL);
+  sigaction(SIGTERM, &sa, NULL);
+
+  int i = 0;
+  while (1)
+    printf("Hello, World!: %i\r", i++);
   return (0);
 }
